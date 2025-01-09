357d9629534da9cef32001739bc6af50b725e255
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2025-01-09 20:05:31 +0000

    adding notes about var

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,6 +10,11 @@ accountEmail = "hc@hc.com"
 accountPassward = "5631123"
 accountCity = "jaipur"
 
+/*
+    prefer not to use var
+    because of issue in block scope and functon scope
+*/
+
 console.log(accountId)
 
 // to print all variables at one line in table form
