3bf25204aec7f65ec2fc7d446ac6d879dfab3388
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2025-01-09 20:34:57 +0000

    learn about datatypes in JS

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS as newer version
+
+// alert(3+3) // gives an error-> we use nodejs , not browser
+
+console.log(3+3)
+
+console.log("Hello")
+
+let name  = "shubh"
+let age = 15
+let isLogedIn = true
+let state;
+
+// string 
+// number
+// boolean -> true/false
+// null -> stand alone value
+// undefined -> when value not assigned
+// symbol -> uniqueness (mostly use in react)
+
+// object
+
+console.log(typeof "shubj")
+console.log(typeof null)
+console.log(typeof undefined)
