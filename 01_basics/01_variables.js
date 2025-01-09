e39eb2d15d205b37abff4ac6ee4468783a27e1cb
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2025-01-09 20:00:16 +0000

    learn variables in javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1456
+let accountEmail = "shubh@gmail.com"
+var accountPassward = "562134"
+accountCity = "Bhopal"
+let accountState;
+
+// accountId = 2314 -> this is not allowed
+
+accountEmail = "hc@hc.com"
+accountPassward = "5631123"
+accountCity = "jaipur"
+
+console.log(accountId)
+
+// to print all variables at one line in table form
+console.table([accountId, accountEmail, accountPassward,accountCity,accountState])
