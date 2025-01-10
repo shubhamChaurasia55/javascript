3405733f8f3c7b79863852a59f5f5bd2ee036d7f
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2025-01-10 19:27:24 +0000

    datatype conversion

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,16 @@
+let someNumber = "gg56"
+
+console.log(someNumber);
+console.log(typeof someNumber)
+
+let stringNumber = Number(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber) // NaN
+
+let isLoggedIn = "kdjs"
+console.log(typeof isLoggedIn)
+let boolLoggedIn = Boolean(isLoggedIn)
+console.log(typeof boolLoggedIn)
+console.log(boolLoggedIn)
+
+
