4b9d806d8da6bb88fa04e7d21152fa7f70a2f42d
Shubham Chaurasia <shubham.sdl7080@gmail.com>
2025-01-10 19:34:59 +0000

    comparison datatype

---
+++ 01_basics/04_dataTypeComparison.js
@@ -0,0 +1,8 @@
+console.log("2" < 1)  // false => convert in number then compare
+console.log("02" < 1) // false
+
+console.log(null == 0)  
+console.log(null > 0)
+console.log(null >= 0)
+
+// in javascript comparison and equality are different
